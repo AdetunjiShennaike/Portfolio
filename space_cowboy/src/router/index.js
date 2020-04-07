e05381296f24323fec8d6a9e61b40af381a08c1a
Adetunji <Ashennaike@gmail.com>
2020-04-06 23:56:01 -0400

    router changed

--- space_cowboy/src/router/index.js
+++ space_cowboy/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Articles from '../views/Articles.vue'
+import Contact from '../views/Contact.vue'
+import Projects from '../views/Projects.vue'
+import Skills from '../views/Skills.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +13,26 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/articles',
+    name: 'Articles',
+    component: Articles
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact
+  },
+  {
+    path: '/projects',
+    name: 'Projects',
+    component: Projects
+  },
+  {
+    path: '/skills',
+    name: 'Skills',
+    component: Skills
   },
   {
     path: '/about',
