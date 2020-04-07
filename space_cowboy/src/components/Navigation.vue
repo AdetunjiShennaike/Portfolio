94319cc1a25dbd9090be9bed4d5fbb7507a305da
Adetunji <Ashennaike@gmail.com>
2020-04-06 23:51:24 -0400

    added social bar and inserted info in it as well as navigation

---
+++ space_cowboy/src/components/Navigation.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="nav" class="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/about">Articles</router-link>
+    <router-link to="/about">Skills</router-link>
+    <router-link to="/about">Projects</router-link>
+    <router-link to="/about">Achievments</router-link>
+    <router-link to="/about">Contact</router-link>
+  </div>
+</template>
+
---
+++ space_cowboy/src/components/SocialMedia.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="social-nav" class="social-nav">
+    <router-link to="#">Instagram</router-link>
+    <router-link to="#">Twitter</router-link>
+    <router-link to="#">Linked In</router-link>
+    <router-link to="#">GitHub</router-link>
+  </div>
+</template>
