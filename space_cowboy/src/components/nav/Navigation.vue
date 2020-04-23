29eb8fc07a3020c39be2e58033f2168bb6ff4197
Adetunji <Ashennaike@gmail.com>
2020-04-23 01:24:33 -0400

    started making the contact form, and changed two icons

--- space_cowboy/src/components/nav/Navigation.vue
+++ space_cowboy/src/components/nav/Navigation.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="nav" class="nav">
     <router-link to="/"><img class="navIcon" :src="require('../../assets/spaceship.png')" /></router-link>
-    <router-link to="/about"><img class="navIcon" :src="require('../../assets/user.png')" /></router-link>
-    <router-link to="/articles"><img class="navIcon" :src="require('../../assets/content.png')" /></router-link>
+    <router-link to="/about"><img class="navIcon" :src="require('../../assets/people.png')" /></router-link>
+    <router-link to="/articles"><img class="navIcon" :src="require('../../assets/blog.png')" /></router-link>
     <router-link to="/skills"><img class="navIcon" :src="require('../../assets/gears.png')" /></router-link>
     <router-link to="/projects"><img class="navIcon" :src="require('../../assets/sketch(1).png')" /></router-link>
     <router-link to="/achievements"><img class="navIcon" :src="require('../../assets/medal.png')" /></router-link>
-    <router-link to="/contact"><img class="navIcon" :src="require('../../assets/mail.png')" /></router-link>
+    <router-link to="/contact"><img class="navIcon" :src="require('../../assets/contact.png')" /></router-link>
   </div>
 </template>
---
+++ space_cowboy/src/views/Contact.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="contact">
+    <h1>Send me a Message</h1>
+    <input required name="name" placeholder="Name" type="text">
+    <input required name="email" placeholder="Email" type="email">
+    <textarea required name="message" rows="5" placeholder="Message"></textarea>
+  </div>
+</template>
